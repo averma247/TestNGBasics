bf055e3fd0756eeb2df24dc1047c7b27b238320a
ajaynagarro <ajay.verma@nagarro.com>
2024-03-14 15:04:14 +0530

    Added gitignore

---
+++ src/main/java/com/pages/amazon/HomePage.java
@@ -0,0 +1,29 @@
+package com.pages.amazon;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class HomePage extends BasePage{
+
+	public HomePage(WebDriver driver, String uiwait, String timeout) {
+		super(driver, uiwait, timeout);
+		// TODO Auto-generated constructor stub
+		PageFactory.initElements(driver, this);
+
+	}
+	
+	@FindBy(id="twotabsearchtextbox")
+	WebElement searchTextBox;
+	
+	@FindBy(id="nav-search-submit-button")
+	WebElement searchButton;
+	
+	public void searchProduct(String searchText) {
+		enterTextInTextBox(searchTextBox, searchText);
+		clickOnButton(searchButton);
+		
+	}
+
+}
--- src/test/java/amazonTesting/AmazonHomePageTesting.java
+++ src/test/java/amazonTesting/AmazonHomePageTesting.java
@@ -1,19 +1,27 @@
 package amazonTesting;
 
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
+import com.pages.amazon.HomePage;
+
 public class AmazonHomePageTesting extends TestBase{
+	
+	HomePage homepage;
+	
+	
+	@BeforeClass
+	public void setup() {
+		
+		homepage= new HomePage(getDriver(), configProp.getProperty("implicitwait"), configProp.getProperty("explicitwait"));
+		
+	}
 	
 	@Test
 	public void verifyHomePage() {
 		
-		System.out.println("Launching and checking Amazon Home Page");
-		try {
-			Thread.sleep(5000);
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+		homepage.searchProduct("iPhone 11");
+		
 	}
 
 }
