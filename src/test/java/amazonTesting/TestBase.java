5f828477ea28e66be26c27a69290672ffe53a485
ajaynagarro <ajay.verma@nagarro.com>
2024-09-07 00:03:36 +0530

    remote ip address added

--- src/test/java/amazonTesting/TestBase.java
+++ src/test/java/amazonTesting/TestBase.java
@@ -100,7 +100,7 @@ public class TestBase {
 		options.addArguments("--disable-extensions");
 		options.addArguments("--disable-application-cache");
 
-		driver = new RemoteWebDriver(new URL("http://172.19.0.2:4444/wd/hub"),options);
+		driver = new RemoteWebDriver(new URL("http://192.168.1.6:4444/wd/hub"),options);
 	}
 
 
